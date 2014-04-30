b4898788c3839246fd888ca855fa8c19ed56822e
Tyler Shelton <sheltotj@rose-hulman.edu>
2014-04-30 14:50:53 -0400

    Added slide handlers

--- wp-content/themes/brightnews/js/mobile.js
+++ wp-content/themes/brightnews/js/mobile.js
@@ -1,3 +1,36 @@
-function slide_left(){
+var transform = ["transform", "msTransform", "webkitTransform", "mozTransform", "oTransform"];
+var width = window.screen.width;
 
+function show_left(){
+	moveMenu("#left-control-pane", width);
+}
+
+function show_right() {
+	moveMenu("#right-control-pane", -width);
+}
+
+function hide_left(){
+	moveMenu("#left-control-pane", -width);
+}
+
+function hide_right() {
+	moveMenu("#right-control-pane", width);
+}
+
+function moveMenu(item, x) {
+	var menu = document.querySelector(item);
+	var transformProperty = getSupportedPropertyName(transform);
+  
+	if (transformProperty) {
+		menu.style[transformProperty] = translate3d(x+"px", 0px, 0px);
+	}
+}
+
+function getSupportedPropertyName(properties) {
+    for (var i = 0; i < properties.length; i++) {
+        if (typeof document.body.style[properties[i]] != "undefined") {
+            return properties[i];
+        }
+    }
+    return null;
 }
